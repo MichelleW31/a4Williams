8d53e36e8315cf701a2bcaf6c455a0f48821f5bb
Michelle Williams <mattie.williams31@gmail.com>
2017-11-21 14:14:44 -0500

    Added CSS

--- scripts/form-component.js
+++ scripts/form-component.js
@@ -2,10 +2,12 @@
 
   var formComponent = {
     template:`
-      <input placeholder = "Noun" ng-model ="$ctrl.words.noun">
-      <input placeholder = "Adjective" ng-model ="$ctrl.words.adj">
-      <input placeholder = "Verb(past tense)" ng-model ="$ctrl.words.verb">
-      <input placeholder = "Adverb" ng-model ="$ctrl.words.adv">
+      <div id = "form">
+        <input placeholder = "Noun" ng-model ="$ctrl.words.noun">
+        <input placeholder = "Adjective" ng-model ="$ctrl.words.adj">
+        <input placeholder = "Verb(ing)" ng-model ="$ctrl.words.verb">
+        <input placeholder = "Adverb" ng-model ="$ctrl.words.adv">
+      </div>
       <button ng-click="$ctrl.setSentence($ctrl.words)">Read Story</button>
     `,
     controller:"FormController"
--- scripts/story-component.js
+++ scripts/story-component.js
@@ -2,8 +2,10 @@
 
   var storyComponent = {
     template:`
-    <p>One fine day, my trusty {{$ctrl.sentence.noun}} woke up to find a {{$ctrl.sentence.adj}} cat. The cat was {{$ctrl.sentence.verb}} {{$ctrl.sentence.adv}}. What a weird morning...</p>
-    <button ng-click="$ctrl.restartStory()">Restart Story</button>
+    <div id="display">
+      <p>One fine day, my trusty {{$ctrl.sentence.noun}} woke up to find a {{$ctrl.sentence.adj}} cat. The cat was {{$ctrl.sentence.verb}} {{$ctrl.sentence.adv}}. What a weird morning...</p>
+      <button ng-click="$ctrl.restartStory()">Restart Story</button>
+    </div>
     `,
     controller: "StoryController"
   }
